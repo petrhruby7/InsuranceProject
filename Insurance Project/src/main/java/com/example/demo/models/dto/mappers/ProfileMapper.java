4c6f1009b35a39a76671ba702fe795395c8d75d1
petrhruby7 <hruby.petr.7@gmail.com>
2024-08-15 11:45:48 +0200

    add ProfoleMapper

--- Insurance Project/src/main/java/com/example/demo/models/dto/mappers/ProfileMapper.java
+++ Insurance Project/src/main/java/com/example/demo/models/dto/mappers/ProfileMapper.java
@@ -1,5 +1,11 @@
 package com.example.demo.models.dto.mappers;
 
+import com.example.demo.data.entities.ProfileEntity;
+import com.example.demo.models.dto.ProfileDTO;
+import org.mapstruct.Mapper;
 
+@Mapper (componentModel = "spring")
 public interface ProfileMapper {
+    ProfileEntity toDTO (ProfileDTO profileDTO);
+    ProfileDTO toEntity (ProfileEntity profileEntity);
 }
