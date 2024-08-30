2d89003add173cf621902a4c6aad6b011ee71351
petrhruby7 <hruby.petr.7@gmail.com>
2024-08-30 22:03:37 +0200

    add createInsurance-Page.html

--- Insurance Project/src/main/java/com/example/demo/data/repositories/InsuranceRepository.java
+++ Insurance Project/src/main/java/com/example/demo/data/repositories/InsuranceRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.data.repositories;
 
+import com.example.demo.data.entities.InsuranceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InsuranceRepository extends JpaRepository {
+public interface InsuranceRepository extends JpaRepository<InsuranceEntity, Long> {
 }
