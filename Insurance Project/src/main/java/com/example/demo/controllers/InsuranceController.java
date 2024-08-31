7aeab0b8e12b41fbb041aa81559bcf3bd921c3e8
petrhruby7 <hruby.petr.7@gmail.com>
2024-08-31 10:26:15 +0200

    add getController

--- Insurance Project/src/main/java/com/example/demo/controllers/InsuranceController.java
+++ Insurance Project/src/main/java/com/example/demo/controllers/InsuranceController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/insurance")
 public class InsuranceController {
+
+    @GetMapping("/")
+    public String renderInsurances(){
+        //todo přidat seznam pojištění
+
+        return "/insurance/myInsurance-Page";
+    }
 }
+
+
+//todo /insurance/create - k vytvoření insurance
+//todo /insurance/ - zobrazení všech pojištění
+//todo /insurance/detail - detail pojištění
+//todo /insurance/update - uprava pojištění
