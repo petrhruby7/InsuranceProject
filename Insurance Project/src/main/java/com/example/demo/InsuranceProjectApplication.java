cc186917e8dda8d1e0bc9b262774cb0804b7c853
petrhruby7 <hruby.petr.7@gmail.com>
2024-09-08 22:46:42 +0200

    CSS forms and buttons

--- Insurance Project/src/main/java/com/example/demo/InsuranceProjectApplication.java
+++ Insurance Project/src/main/java/com/example/demo/InsuranceProjectApplication.java
@@ -17,6 +17,7 @@ public class InsuranceProjectApplication {
 
 //todo: možnost změnit uživatelské heslo
 //todo: po registraci zustan rovnou přihlášený?
+//todo: smazat muj vlastní profil
 
 //todo: updateInsurance-Page - at jde zmenit pojištění i bez start date
 //todo: updateInsurance-Page - at ty type-date maj vyplněnou hodnotu
@@ -24,3 +25,6 @@ public class InsuranceProjectApplication {
 //todo: zobrazit jen eventy přihlášeného uživatele
 
 //todo: chci řadit eventy a insurace dle svého id
+
+//todo: login page - bez chybných hlášek? :O
+//todo: css - aby byli option in create insurance - zelený a ne modrý při výběru
